5be9771096de1eae9ac74bb1fbcf6fc2e408094f
Pablo-Platero <josepabloplaterosanchez@gmail.com>
2025-04-07 14:40:35 -0600

    cambios en stackNavigator.tsx

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { GestureHandlerRefContext } from '@react-navigation/stack'
 import React from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import StackNavigator from './src/navigation/StackNavigator'
+import RegisterScreen from './src/screens/RegisterScreen'
 
 const App = () => {
   return (
     <GestureHandlerRootView style={{flex:1}} >
         <NavigationContainer>
         <StackNavigator />
+        <RegisterScreen />
       </NavigationContainer>
      
     </GestureHandlerRootView>
--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import SplashScreen from '../screens/SplashScreen';
 
 const Stack=createStackNavigator();
 
@@ -11,6 +12,7 @@ const StackNavigator = () => {
     <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name='Login' component={LoginScreen} options={{title:"Iniciar Sesion"}}/>
         <Stack.Screen name='Register' component={RegisterScreen} options={{title:"Registro"}}/>
+        <Stack.Screen name='Splash' component={SplashScreen} options={{title:"Splsh"}}/>
         <Stack.Screen name='Home' component={HomeScreen} options={{title:"Bienvenido"}}/>
 
 
